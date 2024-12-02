5fbc4d367a47a662fc53032fabc18704c1d80ea9
mohamed akmal <muhammedakmal43@gmail.com>
2024-12-02 23:57:49 +0530

    created rough footer and made home screen initial design

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+
+
+const Footer = () => {
+  return (
+    <footer className="bg-black rounded-lg shadow dark:bg-black mt-5">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <hr className="my-6 border-yellow-200 sm:mx-auto dark:border-yellow-600 lg:my-8" />
+        <span className="block text-sm text-400  dark:text-white-400">
+          Developed by Mohamed Akmal
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
